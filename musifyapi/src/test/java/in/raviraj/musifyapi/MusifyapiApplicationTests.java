c5c6ae96575f282f61671b11d2cd9e3ea4c04281
Raviraj <ravishinde96k@gmail.com>
2025-09-09 12:26:09 +0530

    initial commit

---
+++ musifyapi/src/test/java/in/raviraj/musifyapi/MusifyapiApplicationTests.java
@@ -0,0 +1,13 @@
+package in.raviraj.musifyapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusifyapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
