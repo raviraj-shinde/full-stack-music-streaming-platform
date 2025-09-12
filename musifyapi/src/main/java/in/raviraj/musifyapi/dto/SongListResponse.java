c5de579a48d8c5bc37b3d0547a34b0430b53f4a4
Raviraj <ravishinde96k@gmail.com>
2025-09-12 17:37:35 +0530

    SongListResponse and SongRequest DTO's created

--- musifyapi/src/main/java/in/raviraj/musifyapi/dto/SongListResponse.java
+++ musifyapi/src/main/java/in/raviraj/musifyapi/dto/SongListResponse.java
@@ -1,4 +1,18 @@
 package in.raviraj.musifyapi.dto;
 
+import in.raviraj.musifyapi.document.Song;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class SongListResponse {
+    private Boolean success;
+    private List<Song> songs;
 }
