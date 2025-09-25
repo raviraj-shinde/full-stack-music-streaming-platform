440c056221d678739ed7a1b19f96ceb2ab4934d8
Raviraj <ravishinde96k@gmail.com>
2025-09-26 00:27:04 +0530

    fixed issues in JwtUtil and other parts

--- musifyapi/src/main/java/in/raviraj/musifyapi/util/JwtUtil.java
+++ musifyapi/src/main/java/in/raviraj/musifyapi/util/JwtUtil.java
@@ -1,7 +1,6 @@
 package in.raviraj.musifyapi.util;
 
 import io.jsonwebtoken.Claims;
-import io.jsonwebtoken.JwtParser;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.beans.factory.annotation.Value;
